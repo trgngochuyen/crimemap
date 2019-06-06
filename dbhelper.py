cc413e6c36bcf77332e874d82a78ecb71c678975
Truong Ngoc Huyen <trg.ngochuyen@gmail.com>
2019-06-07 02:13:18 +0300

    Fix unexpected indentation

--- dbhelper.py
+++ dbhelper.py
@@ -44,11 +44,11 @@ class DBHelper:
         connection = self.connect()
         try:
             query = "INSERT INTO crimes (category, date, latitude, longitude, description) \ VALUES (%s, %s, %s, %s, %s)"
-               with connection.cursor() as cursor:
-                    cursor.execute(
-                        query, (category, date, latitude, longitude, description))
-                    connection.commit()
-                except Exception as e:
-                    print(e)
-                finally:
-                    connection.close()
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    query, (category, date, latitude, longitude, description))
+                connection.commit()
+            except Exception as e:
+                print(e)
+            finally:
+                connection.close()
