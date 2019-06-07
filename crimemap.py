1c006ca537515d0f9a4520276392918b653c1366
Truong Ngoc Huyen <trg.ngochuyen@gmail.com>
2019-06-07 04:05:24 +0300

    Remove add URL

--- crimemap.py
+++ crimemap.py
@@ -21,16 +21,6 @@ def home():
     return render_template("home.html", data=data)
 
 
-@app.route("/add", methods=["POST"])
-def add():
-    try:
-        data = request.form.get("userinput")
-        DB.add_input(data)
-    except Exception as e:
-        print(e)
-    return home()
-
-
 @app.route("/clear")
 def clear():
     try:
