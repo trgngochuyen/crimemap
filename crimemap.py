9e2f7efc3049aa1fb85e4c64690fa5e1d50bcac2
Truong Ngoc Huyen <trg.ngochuyen@gmail.com>
2019-06-10 14:03:26 +0300

    User input Validation added

--- crimemap.py
+++ crimemap.py
@@ -1,7 +1,10 @@
+import string
 import json
 from flask import request
 from flask import render_template
 from flask import Flask
+import dateparser
+import datetime
 import dbconfig
 if dbconfig.test:
     from mockdbhelper import MockDBHelper as DBHelper
@@ -11,13 +14,14 @@ else:
 
 app = Flask(__name__)
 DB = DBHelper()
+categories = ['mugging', 'break-in']
 
 
 @app.route("/")
-def home():
+def home(error_message=None):
     crimes = DB.get_all_crimes()
     crimes = json.dumps(crimes)
-    return render_template("home.html", crimes=crimes)
+    return render_template("home.html", crimes=crimes, categories=categories, error_message=error_message)
 
 
 @app.route("/clear")
@@ -32,12 +36,33 @@ def clear():
 @app.route("/submitcrime", methods=['POST'])
 def submitcrime():
     category = request.form.get("category")
-    date = request.form.get("date")
-    latitude = float(request.form.get("latitude"))
-    longitude = float(request.form.get("longitude"))
+    if category not in categories:
+        return home()
+    date = format_date(request.form.get("date"))
+    if not date:
+        return home("Invalid date. Please use yyyy-mm-dd format.")
+    try:
+        latitude = float(request.form.get("latitude"))
+        longitude = float(request.form.get("longitude"))
+    except ValueError:
+        return home()
     description = request.form.get("description")
+    description = sanitize_string(request.form.get("description"))
     DB.add_crime(category, date, latitude, longitude, description)
     return home()
+
+
+def format_date(userdate):
+    date = dateparser.parse(userdate)
+    try:
+        return datetime.datetime.strftime(date, "%Y-%m-%d")
+    except TypeError:
+        return None
+
+
+def sanitize_string(userinput):
+    whitelist = string.letters + string.digits + " !?$.,;:-'()&"
+    return filter(lambda x: x in whitelist, userinput)
 
 
 if __name__ == '__main__':
